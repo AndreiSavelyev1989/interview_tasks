b90c35da1dce1c1573c5789a506a46ee6b5b7dee
AndreiSavelyev1989 <andreiscorpion19892010@gmail.com>
2021-07-01 17:26:54 +0300

    receive min and max value from array

---
+++ src/tasks/ReceiveMinMaxValue.js
@@ -0,0 +1,23 @@
+export const ReceiveMinMaxValue = () => {
+    const numbers = [1, 2, 88, 100, -2, 13]
+
+    let minValue = numbers[0]
+    let maxValue = numbers[0]
+    for (let i = 0; i < numbers.length; i++) {
+        if (numbers[i] < minValue) {
+            minValue = numbers[i]
+        }
+        if (numbers[i] > maxValue) {
+            maxValue = numbers[i]
+        }
+    }
+    console.log(`minValue: ${minValue}, maxValue: ${maxValue}`)
+    return (
+        <div>
+            <b>Receive min and max value from array:</b>
+            <div><span>Array: </span>[{numbers.map(num => <span>{`${num}, `}</span>)}]</div>
+            <div><span>Min value</span>{minValue}</div>
+            <div><span>Max value</span>{maxValue}</div>
+        </div>
+    )
+}
